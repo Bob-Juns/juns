7700a6e0a2d6df34c74ba2cbad1f20f8c810e571
Juns Bob <p_juns@kakao.com>
2021-11-29 13:07:17 +0900

    Add cookie-parser and body-parser

--- server/index.js
+++ server/index.js
@@ -6,5 +6,12 @@ const app = express();
 // connect database
 require('./config/mongoose.config');
 
+// cookie parser
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// body-parser
+app.use(express.json());
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
