b41f793be8f6ce57d6f7f369459d0d8692bba792
Juns Bob <p_juns@kakao.com>
2021-11-29 14:32:01 +0900

    Add user register

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,24 @@
 require('dotenv').config();
 
 const router = require('express').Router();
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+
+router.post('/register', async (req, res) => {
+	const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+	try {
+		await new User({
+			userName: req.body.userName,
+			userId: req.body.userId,
+			userEmail: req.body.userEmail,
+			password: hashedPassword,
+		}).save();
+
+		return res.status(201).json({ message: '회원가입 되었습니다.' });
+	} catch {
+		return res.json({ message: '회원가입이 실패했습니다.' });
+	}
+});
 
 module.exports = router;
