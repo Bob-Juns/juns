a3ab3acb693c2bb967a6d6b7131206794cdd7654
Juns Bob <p_juns@kakao.com>
2021-11-29 16:02:57 +0900

    Add auth middleware

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+
+const auth = async (req, res, next) => {
+	const token = req.cookies.authToken;
+	if (token) {
+		try {
+			const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+			const user = await User.findOne({ userEmail: decoded.userEmail });
+			req.user = user;
+			return next();
+		} catch {
+			return res.json({ message: '유효하지 않은 토큰입니다.' });
+		}
+	} else {
+		return res.status(403).json({ message: '토큰이 존재하지 않습니다.' });
+	}
+};
+
+const admin = (req, res, next) => {
+	auth(req, res, () => {
+		if (req.user.isAdmin) return next();
+		return res.json({ message: '관리자만 접근할 수 있습니다.' });
+	});
+};
+
+module.exports = {
+	auth,
+	admin,
+};
