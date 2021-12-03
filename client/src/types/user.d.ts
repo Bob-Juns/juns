fe27ff28b534a68f3169f6aab316cb4480d4869f
Juns Bob <p_juns@kakao.com>
2021-12-03 16:08:55 +0900

    Add types
    [+] user types
    [+] images declarations

---
+++ client/src/types/user.d.ts
@@ -0,0 +1,34 @@
+// common action type
+type Action = {
+  type: string;
+};
+
+type CurrentUser = {
+  userName: string;
+  userId: string;
+  userEmail: string;
+  isAuth: boolean;
+  isAdmin: boolean;
+};
+
+type AllUsers = CurrentUser[];
+
+interface User {
+  currentUser: CurrentUser;
+  allUsers: AllUsers;
+}
+
+interface GetUsers extends Action {
+  payload: Promise<AllUsers>;
+}
+
+interface CheckAuth extends Action {
+  payload: Promise<CurrentUser>;
+}
+
+type UserAction = GetUsers | CheckAuth;
+
+type LoginData = {
+  userEmail: string;
+  password: string;
+};
