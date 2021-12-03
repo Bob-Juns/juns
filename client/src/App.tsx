771377d1f18be3a2e6f081d6f1c2f843c01cdce5
Juns Bob <p_juns@kakao.com>
2021-12-03 16:19:38 +0900

    Update Routes in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import PublicRoute from '@components/Routes/PublicRoute';
+
+import Home from '@pages/Home/Home';
+import Login from '@pages/User/Login/Login';
+
+import Toast from '@components/Toast/Toast';
 
 const App = () => {
-  return <div>App</div>;
+  const appHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', appHeight);
+    appHeight();
+    return () => {
+      window.removeEventListener('resize', appHeight);
+    };
+  }, []);
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PublicRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route path="/login" element={<PublicRoute restricted />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </Router>
+      <Toast />
+    </>
+  );
 };
 
 export default App;
