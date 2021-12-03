318726d486114f8c88edd7247b2d0986abb846ae
Juns Bob <p_juns@kakao.com>
2021-12-03 16:18:36 +0900

    Add toast message

---
+++ client/src/components/Toast/Toast.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { ToastContainer, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Toast = () => {
+  return (
+    <ToastContainer position="top-center" autoClose={2000} transition={Slide} />
+  );
+};
+
+export default Toast;
