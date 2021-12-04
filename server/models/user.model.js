6c9c3ce8ea7746e3851c9fc3a76a0a6b83db0d94
Juns Bob <p_juns@kakao.com>
2021-12-04 15:39:56 +0900

    Update user model
    [+] registerWith
    [*] password to userPassword

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
 	userName: { type: String, required: true },
 	userId: { type: String, required: true, unique: true, trim: true },
 	userEmail: { type: String, required: true, unique: true, trim: true },
-	password: { type: String, required: true },
+	userPassword: { type: String, required: true },
+	registerWith: { type: String, required: true },
 	isAdmin: { type: Boolean, default: false },
 });
 
