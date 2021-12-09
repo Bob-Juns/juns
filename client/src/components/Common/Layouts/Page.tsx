1681eb7439ee827c213e3263de450cc19eaef2d9
Juns Bob <p_juns@kakao.com>
2021-12-09 13:43:16 +0900

    Update page layout

--- client/src/components/Common/Layouts/Page.tsx
+++ client/src/components/Common/Layouts/Page.tsx
@@ -3,23 +3,36 @@ import styled from 'styled-components';
 import Header from '@components/Common/Header/Header';
 
 type Props = {
+  dashboard?: boolean;
   children: React.ReactNode;
 };
 
-const Page = ({ children }: Props) => {
+const Page = ({ dashboard = false, children }: Props) => {
   return (
     <>
-      <Header />
-      <Body>{children}</Body>
+      <Header dashboard={dashboard} />
+      <Body dashboard={dashboard}>{children}</Body>
     </>
   );
 };
 
-const Body = styled.section`
+const Body = styled.div<{ dashboard: boolean }>`
   width: 100vw;
-  min-height: calc(100vh - 5.375rem);
-  min-height: calc(var(--vh, 1vh) * 100 -5.375rem);
-  margin-top: 5.375rem;
+  padding: 1rem 0.5rem;
+  ${(props) =>
+    props.dashboard
+      ? `
+    min-height: calc(100vh - 6.75rem);
+    min-height: calc(var(--vh, 1vh) * 100 - 6.75rem);
+    margin-top: 6.75rem;
+    background-color: ${props.theme.color.gray.light};
+  `
+      : `
+  min-height: calc(100vh - 3.75rem);
+  min-height: calc(var(--vh, 1vh) * 100 - 3.75rem);
+  margin-top: 3.75rem;
+  background-color: #fff;
+  `}
 `;
 
 export default Page;
