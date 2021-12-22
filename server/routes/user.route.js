fafd882fd8cd5ab51318b6a0cc8989f0d3c10660
Juns Bob <p_juns@kakao.com>
2021-12-22 14:32:24 +0900

    Update user router
    [+] withdraw, reset-password
    [*] kakao login

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -144,6 +144,7 @@ router.post('/kakao', async (req, res) => {
 				process.env.SOCIAL_SECRET,
 				'kakao'
 			);
+			await login(res, token);
 		}
 	} catch {
 		return res.status(400).json({ message: '로그인이 실패했습니다.' });
@@ -166,14 +167,23 @@ router.delete('/delete/:userEmail', admin, async (req, res) => {
 	return res.status(200).json({ message: '유저가 삭제되었습니다.' });
 });
 
-router.put('/update-password/', auth, async (req, res) => {
+router.put('/update-password', auth, async (req, res) => {
 	const comparePassword = await bcrypt.compare(
 		req.body.userPassword,
 		req.user.userPassword
 	);
 
+	const checkDuplicate = await bcrypt.compare(
+		req.body.newPassword,
+		req.user.userPassword
+	);
+
 	if (!comparePassword) {
-		return res.status(409).json({ message: '잘못된 비밀번호입니다.' });
+		return res.status(409).json({ message: '기존 비밀번호를 확인해주세요.' });
+	} else if (checkDuplicate) {
+		return res
+			.status(400)
+			.json({ message: '기존 비밀번호로 변경할 수 없습니다.' });
 	} else {
 		const hashedPassword = await bcrypt.hash(req.body.newPassword, 10);
 		await User.findOneAndUpdate(
@@ -181,10 +191,67 @@ router.put('/update-password/', auth, async (req, res) => {
 			{ userPassword: hashedPassword },
 			{ new: true }
 		).orFail(() =>
-			res.status(409).json({ message: '비밀번호 변경이 실패했습니다.' })
+			res.status(404).json({ message: '비밀번호 변경이 실패했습니다.' })
 		);
 		return res.status(200).json({ message: '비밀번호가 변경되었습니다.' });
 	}
 });
-``;
+
+router.put('/update-profile', auth, async (req, res) => {
+	try {
+		await User.findOneAndUpdate(
+			{ userEmail: req.user.userEmail },
+			{ userName: req.body.userName },
+			{ new: true }
+		).orFail(() =>
+			res.status(409).json({ message: '유저정보 변경이 실패했습니다.' })
+		);
+		return res.status(200).json({ message: '유저 정보가 변경되었습니다.' });
+	} catch {
+		return res.status(400).json({ message: '유저정보 변경이 실패했습니다.' });
+	}
+});
+
+router.post('/reset-password', async (req, res) => {
+	const user = await User.findOne({ userEmail: req.body.userEmail }).orFail(
+		() => res.status(404).json({ message: '가입되지 않은 이메일입니다.' })
+	);
+
+	if (user.registerWith === 'kakao') {
+		return res.status(409).json({ message: '카카오로 가입된 이메일입니다.' });
+	} else {
+		const code = generateCode(8);
+		const filePath = path.join(
+			__dirname,
+			'../template',
+			'resetPassword.template.html'
+		);
+
+		await transporter.sendMail(
+			{
+				from: `no-reply <${process.env.NODEMAILER_USER}>`,
+				to: req.body.userEmail,
+				subject: '[JUNSTREAMING] 비밀번호가 재설정되었습니다.',
+				html: htmlToSend(filePath, code),
+			},
+			(err, info) => {
+				if (err) {
+					return res
+						.status(400)
+						.json({ message: '이메일 발송이 실패했습니다.' });
+				} else {
+					return res.status(200).json({ message: '이메일이 발송되었습니다.' });
+				}
+			}
+		);
+	}
+});
+
+router.delete('/withdraw', auth, async (req, res) => {
+	await User.findOneAndDelete({ userEmail: req.user.userEmail }).orFail(() =>
+		res.status(404).json({ message: '서비스 탈퇴가 실패했습니다.' })
+	);
+	return res.status(200).json({ message: '탈퇴 퇴었습니다.' });
+});
+
 module.exports = router;
