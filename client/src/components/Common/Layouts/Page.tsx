83e8d61c44d84c2c1be8d9a50bc42c54b0b9f37a
Juns Bob <p_juns@kakao.com>
2021-12-31 15:27:42 +0900

    Update page layout

--- client/src/components/Common/Layouts/Page.tsx
+++ client/src/components/Common/Layouts/Page.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Header from '@components/Common/Header/Header';
 
 type Props = {
-  dashboard?: boolean;
+  location?: string;
   children: React.ReactNode;
 };
 
-const Page = ({ dashboard = false, children }: Props) => {
+const Page = ({ location = 'default', children }: Props) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
-      <Header dashboard={dashboard} />
-      <Body dashboard={dashboard}>{children}</Body>
+      <Header location={location} />
+      <Body location={location}>{children}</Body>
     </>
   );
 };
 
-const Body = styled.div<{ dashboard: boolean }>`
+const Body = styled.div<{ location: string }>`
   width: 100vw;
   ${(props) =>
-    props.dashboard
+    props.location === 'dashboard' || props.location === 'channels'
       ? `
     min-height: calc(100vh - 6.75rem);
     min-height: calc(var(--vh, 1vh) * 100 - 6.75rem);
@@ -27,14 +30,21 @@ const Body = styled.div<{ dashboard: boolean }>`
     padding: 1rem 0.5rem;
 
     background-color: ${props.theme.color.gray.light};
+    overflow-x: hidden;
   `
+      : props.location === 'detail'
+      ? `min-height: calc(100vh - 3.75rem);
+      min-height: calc(var(--vh, 1vh) * 100 - 3.75rem);
+      margin-top: 3.75rem;
+      background-color: ${props.theme.color.gray.light};`
       : `
   min-height: calc(100vh - 3.75rem);
   min-height: calc(var(--vh, 1vh) * 100 - 3.75rem);
-  margin-top: 3.75rem;
-  padding: 1rem 0;
-  background-color: #fff;
+  margin-top: 4.75rem;
+
+
   position: relative;
+  background-color: #fff;
   `}
 `;
 
